de5336fab7eb42d9cea20ab5264528180688f53c
bmbz <elvin007pires@gmail.com>
2026-05-13 13:05:59 -0100

    correcao no teste de conexao no server.js, e mudanca na porta do .env

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 // Teste de conexão simples
 async function testConnection() {
     try {
-        
+        const db = require('./backend/src/config/db');
+        await db.query('SELECT 1');
         console.log('✅ Conexão com o MySQL estabelecida com sucesso!');
     } catch (err) {
         console.error('❌ Erro ao conectar à base de dados:', err.message);
@@ -16,7 +17,7 @@ async function testConnection() {
 
 testConnection();
 
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor a rodar na porta ${PORT}`);
 });
