b743a7cb4ed0f198b93cfe6378e5aa1bf9a1823b
bmbz <elvin007pires@gmail.com>
2026-05-13 14:02:42 -0100

    correcao  de alguns erros(nomeadamente categprias -> categorias)

--- backend/src/models/book.model.js
+++ backend/src/models/book.model.js
@@ -1,19 +1,19 @@
 const pool = require('../../../backend/src/config/db')
 
 async function listarLivros() {
-    const query = "SELECT livros.*, categprias.nome FROM livros LEFT JOIN categorias ON livros.categoria_id = categorias.id ORDER BY livros.categoria_id";
+    const query = "SELECT livros.*, categorias.nome FROM livros LEFT JOIN categorias ON livros.categoria_id = categorias.id ORDER BY livros.categoria_id";
     const result= await pool.query(query);
     return result.rows;
 }
 
 async function obterLivroPorId(id) {
-    const query = "SELECT livros.*, categprias.nome FROM livros LEFT JOIN categorias ON livros.categoria_id = categorias.id WHERE livros.id = $1";
+    const query = "SELECT livros.*, categorias.nome FROM livros LEFT JOIN categorias ON livros.categoria_id = categorias.id WHERE livros.id = $1";
     const result= await pool.query(query, [id]);
     return result.rows[0];
 }
 
 async function obterLivroPorCat(cat){
-    const query = "SELECT livros.*, categprias.nome FROM livros LEFT JOIN categorias ON livros.categoria_id = categorias.id ORDER BY livros.id WHERE livros.categoria_id = $1";
+    const query = "SELECT livros.*, categorias.nome FROM livros LEFT JOIN categorias ON livros.categoria_id = categorias.id ORDER BY livros.id WHERE livros.categoria_id = $1";
     const result= await pool.query(query, cat);
     return result.rows;
 }
